d544a35b6ae0172808bc63c396402bd4dcdd07ae
Kristof Jozsa <kristof.jozsa@gmail.com>
2024-06-03 16:23:07 +0200

    cleanup LoanDisbursementDetailsAssembler

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/service/LoanDisbursementDetailsAssembler.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/service/LoanDisbursementDetailsAssembler.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -20,16 +20,17 @@ package org.apache.fineract.portfolio.loanaccount.service;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.fineract.infrastructure.core.serialization.FromJsonHelper;
 import org.apache.fineract.portfolio.loanaccount.api.LoanApiConstants;
 import org.apache.fineract.portfolio.loanaccount.domain.LoanDisbursementDetails;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 @RequiredArgsConstructor
 public class LoanDisbursementDetailsAssembler {
@@ -40,44 +41,37 @@ public class LoanDisbursementDetailsAssembler {
         final Locale locale = this.fromApiJsonHelper.extractLocaleParameter(command);
         final String dateFormat = this.fromApiJsonHelper.extractDateFormatParameter(command);
         List<LoanDisbursementDetails> disbursementDatas = new ArrayList<>();
-        if (command.has(LoanApiConstants.disbursementDataParameterName)) {
-            final JsonArray disbursementDataArray = command.getAsJsonArray(LoanApiConstants.disbursementDataParameterName);
-            if (disbursementDataArray != null && !disbursementDataArray.isEmpty()) {
-                int i = 0;
-                do {
-                    final JsonObject jsonObject = disbursementDataArray.get(i).getAsJsonObject();
-                    LocalDate expectedDisbursementDate = null;
-                    LocalDate actualDisbursementDate = null;
-                    BigDecimal principal = null;
-                    BigDecimal netDisbursalAmount = null;
 
-                    if (jsonObject.has(LoanApiConstants.expectedDisbursementDateParameterName)) {
-                        expectedDisbursementDate = this.fromApiJsonHelper.extractLocalDateNamed(
-                                LoanApiConstants.expectedDisbursementDateParameterName, jsonObject, dateFormat, locale);
-                    }
-                    if (jsonObject.has(LoanApiConstants.disbursementPrincipalParameterName)
-                            && jsonObject.get(LoanApiConstants.disbursementPrincipalParameterName).isJsonPrimitive()
-                            && StringUtils.isNotBlank(jsonObject.get(LoanApiConstants.disbursementPrincipalParameterName).getAsString())) {
-                        principal = jsonObject.getAsJsonPrimitive(LoanApiConstants.disbursementPrincipalParameterName).getAsBigDecimal();
-                    }
-                    if (jsonObject.has(LoanApiConstants.disbursementNetDisbursalAmountParameterName)
-                            && jsonObject.get(LoanApiConstants.disbursementNetDisbursalAmountParameterName).isJsonPrimitive()
-                            && StringUtils.isNotBlank(
-                                    jsonObject.get(LoanApiConstants.disbursementNetDisbursalAmountParameterName).getAsString())) {
-                        netDisbursalAmount = jsonObject.getAsJsonPrimitive(LoanApiConstants.disbursementNetDisbursalAmountParameterName)
-                                .getAsBigDecimal();
-                    }
-                    boolean isReversed = false;
-                    if (jsonObject.has(LoanApiConstants.disbursementReversedParameterName)) {
-                        isReversed = this.fromApiJsonHelper.extractBooleanNamed(LoanApiConstants.disbursementReversedParameterName,
-                                jsonObject);
-                    }
-                    disbursementDatas.add(new LoanDisbursementDetails(expectedDisbursementDate, actualDisbursementDate, principal,
-                            netDisbursalAmount, isReversed));
-                    i++;
-                } while (i < disbursementDataArray.size());
-            }
+        JsonArray disbursementDataArray = command.getAsJsonArray(LoanApiConstants.disbursementDataParameterName);
+        if (disbursementDataArray != null && !disbursementDataArray.isEmpty()) {
+            disbursementDataArray.forEach(jsonElement -> {
+                JsonObject jsonObject = jsonElement.getAsJsonObject();
+
+                LocalDate expectedDisbursementDate = jsonObject.has(LoanApiConstants.expectedDisbursementDateParameterName)
+                        ? this.fromApiJsonHelper.extractLocalDateNamed(LoanApiConstants.expectedDisbursementDateParameterName,
+                        jsonObject, dateFormat, locale)
+                        : null;
+
+                BigDecimal principal = jsonObject.has(LoanApiConstants.disbursementPrincipalParameterName)
+                        && jsonObject.get(LoanApiConstants.disbursementPrincipalParameterName).isJsonPrimitive()
+                        && StringUtils.isNotBlank(jsonObject.get(LoanApiConstants.disbursementPrincipalParameterName).getAsString())
+                        ? jsonObject.getAsJsonPrimitive(LoanApiConstants.disbursementPrincipalParameterName).getAsBigDecimal()
+                        : null;
+
+                BigDecimal netDisbursalAmount = jsonObject.has(LoanApiConstants.disbursementNetDisbursalAmountParameterName)
+                        && jsonObject.get(LoanApiConstants.disbursementNetDisbursalAmountParameterName).isJsonPrimitive()
+                        && StringUtils.isNotBlank(jsonObject.get(LoanApiConstants.disbursementNetDisbursalAmountParameterName).getAsString())
+                        ? jsonObject.getAsJsonPrimitive(LoanApiConstants.disbursementNetDisbursalAmountParameterName).getAsBigDecimal()
+                        : null;
+
+                boolean isReversed = jsonObject.has(LoanApiConstants.disbursementReversedParameterName)
+                        ? this.fromApiJsonHelper.extractBooleanNamed(LoanApiConstants.disbursementReversedParameterName, jsonObject)
+                        : false;
+
+                disbursementDatas.add(new LoanDisbursementDetails(expectedDisbursementDate, null, principal, netDisbursalAmount, isReversed));
+            });
         }
+
         return disbursementDatas;
     }
 }
